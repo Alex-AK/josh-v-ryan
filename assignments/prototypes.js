9f8a505886daec1cceeb19db85b6770853d5e573
David Flack <davidscottflack@gmail.com>
2018-11-30 12:41:40 -0600

    Refactor to ES6 class notation.

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -1,38 +1,72 @@
-function GameObject(gameAttributes) {
-  this.createdAt = new Date('November 28, 2018 03:24:00');
-  this.dimensions = gameAttributes.dimensions;
-  this.destroy = function () {
+// function GameObject(gameAttributes) {
+//   this.createdAt = new Date('November 28, 2018 03:24:00');
+//   this.dimensions = gameAttributes.dimensions;
+//   this.destroy = function () {
+//     alert(`${this.name} was removed from the game.`);
+//   }
+// }
+
+class GameObject {
+  constructor(gameAttributes) {
+    this.createdAt = new Date('November 28, 2018 03:24:00');
+    this.dimensions = gameAttributes.dimensions;
+  }
+  destroy() {
     alert(`${this.name} was removed from the game.`);
   }
 }
 
+// function CharacterStats(characterAttibutes) {
+//   GameObject.call(this, characterAttibutes);
+//   this.healthPoints = characterAttibutes.healthPoints;
+//   this.name = characterAttibutes.name;
+//   this.takeDamage = function () {
+//     alert(`${this.name}: OUCH!`);
+//   }
+//   this.death = function () {
+//     if (this.healthPoints === 0) {
+//       return `TEST`;
+//     }
+//   }
+// }
 
-
-function CharacterStats(characterAttibutes) {
-  GameObject.call(this, characterAttibutes);
-  this.healthPoints = characterAttibutes.healthPoints;
-  this.name = characterAttibutes.name;
-  this.takeDamage = function () {
+class CharacterStats extends GameObject {
+  constructor(characterAttibutes) {
+    super(characterAttibutes);
+    this.healthPoints = characterAttibutes.healthPoints;
+    this.name = characterAttibutes.name;
+  }
+  takeDamage() {
     alert(`${this.name}: OUCH!`);
   }
-  this.death = function () {
+  death() {
     if (this.healthPoints === 0) {
-      return `TEST`;
+        return `TEST`;
     }
+  }// death()
+}// CharacterStats
+
+// function Humanoid(humanoidAttributes) {
+//   CharacterStats.call(this, humanoidAttributes);
+//   this.team = humanoidAttributes.team;
+//   this.weapons = humanoidAttributes.weapons;
+//   this.language = humanoidAttributes.language;
+//   this.greet = function () {
+//     return `${this.name} offers a greeting in ${this.language}.`
+//   }
+// }
+
+class Humanoid extends CharacterStats {
+  constructor(humanoidAttributes) {
+    super(humanoidAttributes);
+    this.team = humanoidAttributes.team;
+    this.weapons = humanoidAttributes.weapons;
+    this.language = humanoidAttributes.language;
+  }
+  greet() {
+    return (`${this.name} offers a greeting in ${this.language}.`);
   }
 }
-
-
-function Humanoid(humanoidAttributes) {
-  CharacterStats.call(this, humanoidAttributes);
-  this.team = humanoidAttributes.team;
-  this.weapons = humanoidAttributes.weapons;
-  this.language = humanoidAttributes.language;
-  this.greet = function () {
-    return `${this.name} offers a greeting in ${this.language}.`
-  }
-}
-
 
 // const damage = function (opponent) {
 //   opponent.takeDamage();
@@ -43,31 +77,57 @@ function Humanoid(humanoidAttributes) {
 // }
 
 
-function Villian(villianAttributes) {
-  Humanoid.call(this, villianAttributes);
-  this.gingerBomb = function (opponent) {
+// function Villain(villainAttributes) {
+//   Humanoid.call(this, villainAttributes);
+//   this.gingerBomb = function (opponent) {
+//     opponent.takeDamage();
+//     --opponent.healthPoints;
+//     if (opponent.healthPoints === 0) {
+//       opponent.destroy();
+//     }
+//   }
+//   //this.gingerBomb = damage();
+// }
+
+class Villain extends Humanoid {
+  constructor(villainAttributes) {
+    super(villainAttributes);
+  }
+  gingerBomb(opponent) {
     opponent.takeDamage();
     --opponent.healthPoints;
     if (opponent.healthPoints === 0) {
       opponent.destroy();
     }
   }
-  //this.gingerBomb = damage();
 }
 
-function Hero(heroAttributes) {
-  Humanoid.call(this, heroAttributes);
-  this.banjoBomb = function (opponent) {
+// function Hero(heroAttributes) {
+//   Humanoid.call(this, heroAttributes);
+//   this.banjoBomb = function (opponent) {
+//     opponent.takeDamage();
+//     --opponent.healthPoints;
+//     if (opponent.healthPoints === 0) {
+//       opponent.destroy();
+//     }
+//   }
+//   // this.banjoBomb = damage();
+// }
+
+class Hero extends Humanoid {
+  constructor(heroAttributes) {
+    super(heroAttributes);
+  }
+  banjoBomb(opponent) {
     opponent.takeDamage();
     --opponent.healthPoints;
     if (opponent.healthPoints === 0) {
       opponent.destroy();
-    }
-  }
-  // this.banjoBomb = damage();
+    }// no health conditional statement
+  }// banjoBomb
 }
 
-const ryan = new Villian({
+const ryan = new Villain({
   createdAt: new Date(),
   dimensions: {
     length: 2,
